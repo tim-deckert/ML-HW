1275a5bbf386c496a478278f225d4b995b98a8c6
timmy <tadeckert7@gmail.com>
2017-09-21 09:23:52 -0500

    Training done for gaussians begun to work on training histograms

--- naive_bayes.m
+++ naive_bayes.m
@@ -7,7 +7,18 @@ function [] = naive_bayes( data, test, type, number )
     
     sorted_data = sortrows (training_data, width);
     
-    gaussians( sorted_data, height, width );
+    classes = unique (training_data(:,end));
+    
+%     sorted_data(sorted_data(:,width) == 1,:)
+%     size(classes)
+    
+    data_split_by_class = cell(size(classes));
+    
+    for i = 1:size(classes,1)   
+        data_split_by_class{i} = sorted_data(sorted_data(:,width) == classes(i), 1:width-1);
+    end
+         
+    histograms( data_split_by_class, height, width );
     
 %     if nargin == 3 && type == 'gaussians'
 %         gaussians( sorted_data, height, width);
@@ -20,37 +31,80 @@ function [] = naive_bayes( data, test, type, number )
 end
 
 function [] = gaussians( training_data, height, width )
-    avg = mean(training_data(:,1:end-1));
-    std_dev = std(training_data(:,1:end-1));
-    for i = 1:size(std_dev,2)
-        if std_dev(i) < 0.01
-            std_dev(i) = 0.01;
-        end
-    end
+%     avg = mean(training_data(:,1:end-1));
+%     std_dev = std(training_data(:,1:end-1));
+%     for i = 1:size(std_dev,2)
+%         if std_dev(i) < 0.01
+%             std_dev(i) = 0.01;
+%         end
+%     end
+%     
+%     classes = zeros (1, unique (training_data(end)));
+    avgs = zeros (width-1, size(training_data, 1));
+    std_devs = zeros (width-1, size(training_data, 1));
+%     
+%     j = 1;
+%     k = 1;
     
-    classes = zeros (2 * (width-1), unique (training_data(end)));
-    avgs = zeros (width-1, size(classes));
-    std_devs = zeros (width-1, size(classes));
-    
-    j = 1;
-    k = 1;
-    
+%     for i = 1:size(training_data, 1)
+%         
+%         if training_data(i,end) ~= j
+%             avgs(:,j) = mean(training_data(k:i-1,1:width-1));
+%             std_devs(:,j) = std(training_data(k:i-1,1:width-1));
+%             
+% %             mean(training_data(k:i-1,1:end-1))
+% %             training_data(k:i-1,1:end);
+%             j = j + 1;
+%             k = i;
+%         end
+%         classes(j) = classes(j) + 1;
+%         
+%     end
+      
     for i = 1:size(training_data, 1)
         
-        if training_data(i,end) ~= j
-            classes(,j) = mean(training_data(k:i-1,1:end-1));
+        avgs(:,i) = mean(training_data{i});
+        std_devs(:,i) = std(training_data{i});
+            
+%             mean(training_data(k:i-1,1:end-1))
 %             training_data(k:i-1,1:end);
-            j = j + 1;
-            k = i;
-        end
-        classes(1,j) = classes(1,j) + 1;
+%         j = j + 1;
+%         k = i;
+%         
         
-    end
+    end  
     
-    classes(1,:) = classes(1,:) / height;
+    
+%     avgs(:,j) = mean(training_data(k:height,1:width-1));
+%     std_devs(:,j) = std(training_data(k:height,1:width-1));
+    
+    for i = 1:size(std_devs,2)
+        for j = 1:size(std_devs,1)
+            if std_devs(j,i) < 0.01
+                std_devs(j,i) = 0.01;
+            end
+            fprintf("Class %d, attribute %d, mean = %.2f, std = %.2f\n", i, j, avgs(j,i), std_devs(j,i));
+        end
+    end
+%     
+%     classes = classes / height;
+%     for i = 1:size(classes,2)
+%         for
+%         
+%     end
+
             
 end
 
 function [] = histograms( training_data, height, width, number )
 
+    for i = 1:size(training_data, 1)
+        for j = 1:(width-1)
+            bin_data = sortrows (training_data{i}, j);
+            a = bin_data(1,j);
+            b = bin_data(end,j);
+            
+        end
+
+    end
 end
